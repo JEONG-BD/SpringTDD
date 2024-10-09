d5eb977ff74f2b07d7545f6f6c6cba89a15e6be5
JEONG-BD <dev.byeongdu@gmail.com>
2024-10-09 16:35:31 +0900

    test

--- sec01/src/main/java/com/example/sec01/DemoUtils.java
+++ sec01/src/main/java/com/example/sec01/DemoUtils.java
@@ -2,7 +2,24 @@ package com.example.sec01;
 
 public class DemoUtils {
 
+    private static DemoUtils instance = new DemoUtils();
+
+    private DemoUtils() {
+
+    }
+
+    public static DemoUtils getInstance(){
+        return instance;
+    }
+
     public int add(int a, int b) {
         return a + b;
-    }b
+    }
+
+    public Object checkNull(Object obj){
+        if (obj != null){
+            return obj;
+        }
+        return null;
+    }
 }
--- sec01/src/test/java/com/example/sec01/DemoUtilsTests.java
+++ sec01/src/test/java/com/example/sec01/DemoUtilsTests.java
@@ -1,6 +1,7 @@
 package com.example.sec01;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -9,10 +10,21 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class DemoUtilsTests {
 
+    private final DemoUtils demoUtils = DemoUtils.getInstance();
     @Test
     public void testEqualsAndNotEquals() throws Exception{
-        DemoUtils demoUtils = new DemoUtils();
+        //DemoUtils demoUtils = new DemoUtils();
         assertEquals(6, demoUtils.add(2, 4), "2 + 5 must be 6");
         assertNotEquals(6, demoUtils.add(1, 9), "1+9 must not be 6");
+    }
+
+    @Test
+    public void testNullAndNotNull() throws Exception{
+        //DemoUtils demoUtils = new DemoUtils();
+        String str1 = null;
+        String str2 = "tdd";
+
+        assertNull(demoUtils.checkNull(str1), "Object should be Null");
+        assertNotNull(demoUtils.checkNull(str2), "Object should be Null");
     }
 }
