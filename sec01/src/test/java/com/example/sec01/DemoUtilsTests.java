92a32557cc8cf5caaf689c6c314c32f21e92ed08
JEONG-BD <dev.byeongdu@gmail.com>
2024-10-09 16:47:26 +0900

    test lifecycle

--- sec01/src/test/java/com/example/sec01/DemoUtilsTests.java
+++ sec01/src/test/java/com/example/sec01/DemoUtilsTests.java
@@ -1,8 +1,6 @@
 package com.example.sec01;
 
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -10,12 +8,36 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class DemoUtilsTests {
 
+    /*DemoUtils demoUtils ;*/
     private final DemoUtils demoUtils = DemoUtils.getInstance();
+
+    @BeforeEach
+    void setUpBeforeEach(){
+        System.out.println("@Before Each executes befores the execution of each test method");
+    }
+
+    @AfterEach
+    void tearDownAfterEach(){
+        System.out.println("Running @AfterEach");
+    }
+
+    @BeforeAll
+    static void setUpBeforeEachClass(){
+        System.out.println("@BeforeAll");
+    }
+
+
+    @AfterAll
+    static void tearDownAfterAll(){
+        System.out.println("@AfterAll");
+    }
+
     @Test
     public void testEqualsAndNotEquals() throws Exception{
         //DemoUtils demoUtils = new DemoUtils();
         assertEquals(6, demoUtils.add(2, 4), "2 + 5 must be 6");
         assertNotEquals(6, demoUtils.add(1, 9), "1+9 must not be 6");
+        System.out.println("Running testEqualsAndNotEquals");
     }
 
     @Test
@@ -26,5 +48,7 @@ public class DemoUtilsTests {
 
         assertNull(demoUtils.checkNull(str1), "Object should be Null");
         assertNotNull(demoUtils.checkNull(str2), "Object should be Null");
+        System.out.println("Running testNullAndNotNull");
+
     }
 }
