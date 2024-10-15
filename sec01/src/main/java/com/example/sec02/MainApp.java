e428839ae2dfba94e7f8ac218c4d7dee4562cec8
JEONG-BD <dev.byeongdu@gmail.com>
2024-10-16 08:13:54 +0900

    FizzBuzz Main

---
+++ sec01/src/main/java/com/example/sec02/MainApp.java
@@ -0,0 +1,10 @@
+package com.example.sec02;
+
+public class MainApp {
+    public static void main(String[] args) {
+        for (int i = 0; i <=100 ; i++) {
+            //System.out.println(i + "," + FizzBuzz.computer(i));
+            System.out.println(FizzBuzz.computer(i));
+        }
+    }
+}
