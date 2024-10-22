a34e9d39bfdb791ed8c0e2bd40791355dc4c8bee
JEONG-BD <dev.byeongdu@gmail.com>
2024-10-22 10:38:06 +0900

    Bean

--- sec03/src/main/java/com/example/sec03/Sec03Application.java
+++ sec03/src/main/java/com/example/sec03/Sec03Application.java
@@ -1,7 +1,12 @@
 package com.example.sec03;
 
+import com.example.sec03.dao.ApplicationDao;
+import com.example.sec03.models.CollegeStudent;
+import com.example.sec03.service.ApplicationService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Scope;
 
 @SpringBootApplication
 public class Sec03Application {
@@ -10,4 +15,21 @@ public class Sec03Application {
         SpringApplication.run(Sec03Application.class, args);
     }
 
+    @Bean(name="applicationExample")
+    ApplicationService getApplicationService(){
+        return new ApplicationService();
+    }
+
+
+    @Bean(name="applicationDao")
+    ApplicationDao getApplicationDao(){
+        return new ApplicationDao();
+    }
+
+
+    @Bean(name="collegeStudent")
+    @Scope(value="prototype")
+    CollegeStudent getCollegeStudent(){
+        return new CollegeStudent();
+    }
 }
