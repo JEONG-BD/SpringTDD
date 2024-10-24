c618fbae596bfbacaffcfec15e9488630d4f80b2
JEONG-BD <dev.byeongdu@gmail.com>
2024-10-25 08:27:35 +0900

    application.properties

---
+++ sec04/src/test/java/com/example/sec04/Sec04ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sec04;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sec04ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
