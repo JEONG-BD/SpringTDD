8ef22351d6e1e80fbb1da1544e10904d630eb84f
JEONG-BD <dev.byeongdu@gmail.com>
2024-10-28 08:11:44 +0900

    grade interface

---
+++ sec04/src/main/java/com/example/sec04/domain/Grade.java
@@ -0,0 +1,16 @@
+package com.example.sec04.domain;
+
+public interface Grade {
+
+    double getGrade();
+
+    void setId();
+
+    int getStudentId();
+
+    void setStudentId(int studentId);
+
+    void setGrade(double grade);
+
+
+}
